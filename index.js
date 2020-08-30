367f2907394f62d0c1079b841de4fc0069b8cc29
Martin Batista - Full Stack Developer <martdev1963@gmail.com>
2020-08-30 09:41:24 -0400

    js for dice game
    js for dice game

---
+++ index.js
@@ -0,0 +1,33 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1 -6
+
+//alert(randomNumber1);
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png through dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; // folder with the images...
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2  = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🤠Player 1 Wins!!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "😀Player 2 Wins!!";
+}
+else {
+  document.querySelector("h1").innerHTML = "🌞DRAW!!";
+}
+
+function rollTheDice(){
+    location.reload();
+}
